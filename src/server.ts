c658e5d9824c78d29de13a9e49acecb299acdc38
sowmya <sowmya.t@everest.engineering>
2026-02-03 22:36:41 +0530

    Update the services for add the router end point

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './routers/routes';
 
 
 mongoose.connect("mongodb://localhost:27017/order_managements")
     const app = express();
     app.use(express.json());
+    app.use("/", router)
     const PORT = process.env.PORT
+
+
     app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
 });

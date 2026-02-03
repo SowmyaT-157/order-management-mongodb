cb65f9a9bada85fa256cd85946d41fbb05d9ef23
sowmya <sowmya.t@everest.engineering>
2026-02-03 23:28:30 +0530

    Update the controller to fetch all tasks

--- src/controllers/product.ts
+++ src/controllers/product.ts
@@ -1,5 +1,5 @@
 import { Request,Response } from "express"
-import { newProduct } from "../services/product"
+import { getAllProducts, newProduct } from "../services/product"
 
 export const createNewProduct = async (req: Request, res: Response) => {
     try {
@@ -16,3 +16,22 @@ export const createNewProduct = async (req: Request, res: Response) => {
         return res.status(500).json({ message: "network issue", error })
     }
 }
+
+
+export const getProducts = async (req: Request, res: Response) => {
+    
+    try {
+        console.log("enter try block..");
+        const dataOfProduct = await getAllProducts();
+        console.log(dataOfProduct,"product data..");
+        if (!dataOfProduct) {
+            console.log(dataOfProduct,"comming if condition");
+            return res.status(404).json({ message: "data is not there" });
+        } else {
+            return res.status(200).json({ message: "fetched the data", dataOfProduct });
+        }
+    } catch {
+        return res.status(400).json({ message: "data is not fetching" });
+    }
+};
+

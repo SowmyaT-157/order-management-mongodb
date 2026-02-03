393f5aad8e9ff66b0ae15c367523204a1b4c840d
sowmya <sowmya.t@everest.engineering>
2026-02-03 23:30:32 +0530

    Add the services to fetch all tasks

--- src/services/product.ts
+++ src/services/product.ts
@@ -5,3 +5,8 @@ export const newProduct = async (productData:productType) => {
   const product = await Product.create(productData)
   return product;
 }
+
+export const getAllProducts = async() =>{
+    const allProducts = await Product.find()
+    return allProducts
+}

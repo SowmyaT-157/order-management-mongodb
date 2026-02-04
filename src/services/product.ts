12060f65cf7d94807350d4f5230827d861f91248
sowmya <sowmya.t@everest.engineering>
2026-02-04 19:17:21 +0530

    Update the services for add the total revenue

--- src/services/product.ts
+++ src/services/product.ts
@@ -14,28 +14,29 @@ export const getAllProducts = async () => {
 
 
 
-export const createOrder = async (newOrderData: orderType) => {
 
-  const productUpdate = await Product.findOneAndUpdate(
-    {
-      _id: newOrderData.product_id,
-      quantity: { $gte: newOrderData.order_quantity }
-    },
-    { $inc: { quantity: -newOrderData.order_quantity } },
-    { new: true }
-  );
-  if (!productUpdate) {
-    console.log("product is out of stock")
-  } else {
-    return await Orders.create(newOrderData);
+export const createOrder = async (newOrderData:Omit<orderType,'order_amount'>) => {
+
+  const productData = await Product.findById(newOrderData.product_id)
+  if(!productData){
+    console.log("product id is not found")
+  }else{
+  if(newOrderData.order_quantity>0 && newOrderData.order_quantity <= productData.quantity!){
+    productData.quantity! -= newOrderData.order_quantity ;
+    productData.save();
+    let data = newOrderData.order_quantity * productData.price
+   return await Orders.create({...newOrderData,order_amount:data});
   }
-};
+  }
+}
+
+
 
 export const cancelTheOrder = async (order_id: string) => {
     const orderData = await Orders.findOneAndUpdate(
         { _id: order_id, is_active: 'active' },
         { $set: { is_active: 'cancelled' } },
-        { new: true }
+        { returnNewDocument: true }
     );
     if (!orderData){
       console.log("order cancelled")
@@ -45,9 +46,27 @@ export const cancelTheOrder = async (order_id: string) => {
         { $inc: { quantity: orderData.order_quantity } })
     return updatedOrder;
 };
-
 }
+
+
 
 export const getTheOrders = async () => {
     return await Orders.find({ is_active: 'active' });
 };
+
+
+
+export const getReportStats = async () => {
+   
+    const revenueData = await Orders.aggregate([
+        { $match: { is_active: "active" } }, 
+        
+        { 
+            $group: { 
+                _id: null, 
+                totalRevenue: { $sum: { $multiply: ["$order_quantity", "$order_amount"] } } 
+            } 
+        }
+    ]);
+ return revenueData
+}
